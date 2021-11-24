ab05e7c76eddcdb077f1fa02f219db3779f7a268
underscoren <marius.steffens@outlook.com>
2021-11-24 22:25:02 +0000

    fixed some quotes

--- app-src/content/components.js
+++ app-src/content/components.js
@@ -22,10 +22,10 @@ function TagList(props) {
 
 function Card(props) {
     return (
-    <div className='col mb-3'>
-        <div className='card'>
-            <div className='card-body'>
-                <h5 className='card-title'>{props.title}</h5>
+    <div className="col mb-3">
+        <div className="card">
+            <div className="card-body">
+                <h5 className="card-title">{props.title}</h5>
                 {props.children}
             </div>
         </div>
